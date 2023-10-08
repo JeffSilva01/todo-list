92446f739e4973bac6d6aec31621cacd3908549e
Jefferson Silva <jeffsilva01.dev@gmail.com>
2023-10-07 22:37:53 -0300

    refactor code

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,16 +2,16 @@ import { InputHTMLAttributes, forwardRef } from 'react'
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
-const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => (
-  <input
-    className="w-full rounded-lg border border-gray-700 bg-gray-500 p-4 text-gray-300 outline-none placeholder:text-gray-300 focus:border-purple-700"
-    placeholder="Adicione uma nova tarefa"
-    type="text"
-    ref={ref}
-    {...props}
-  />
-))
-
-Input.displayName = 'Input'
-
-export { Input }
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  function Input(props, ref) {
+    return (
+      <input
+        className="w-full rounded-lg border border-gray-700 bg-gray-500 p-4 text-gray-300 outline-none placeholder:text-gray-300 focus:border-purple-700"
+        placeholder="Adicione uma nova tarefa"
+        type="text"
+        ref={ref}
+        {...props}
+      />
+    )
+  },
+)
